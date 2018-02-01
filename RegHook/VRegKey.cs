14fc581dce5381bb7587690ced9217a47182ef0f
Tony Su <peitaosu@163.com>
2018-02-01 21:15:21 +0800

    add Virtual Registry classes

---
+++ RegHook/VRegKey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegHook
+{
+    public class VRegKey
+    {
+        public Dictionary<string, VRegKey> Keys { set; get; }
+        public List<VRegValue> Values { set; get; }
+    }
+
+    public class VRegValue
+    {
+        public string Name { set; get; }
+        public string Type { set; get; }
+        public string Data { set; get; }
+    }
+}
