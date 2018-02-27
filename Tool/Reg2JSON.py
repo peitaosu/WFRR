f23152014cff387081f4ebad72ec490db8b7b5c4
Tony Su <peitaosu@163.com>
2018-02-27 22:42:19 +0800

    add reg key convert implement

--- Tool/Reg2JSON.py
+++ Tool/Reg2JSON.py
@@ -6,12 +6,25 @@ def get_reg_str_list(reg_file_path):
     return filter(None, reg)
 
 def convert_reg_str_to_json(reg_str_list):
-    reg_dict = {}
+    reg_dict = {
+        "Keys": {},
+        "Values": []
+    }
     for reg_str in reg_str_list:
         if reg_str == 'Windows Registry Editor Version 5.00':
             continue
         if reg_str.startswith('['):
-            continue
+            reg_str = reg_str[1:-1]
+            cur_dict = reg_dict
+            for reg_key in reg_str.split('\\'):
+                if reg_key not in cur_dict['Keys'].keys():
+                    cur_dict['Keys'][reg_key] = {
+                        "Keys": {},
+                        "Values": []
+                    }
+                    cur_dict = cur_dict['Keys'][reg_key]
+                else:
+                    cur_dict = cur_dict['Keys'][reg_key]
         else:
             continue
     return reg_dict
