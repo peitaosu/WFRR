d2d93c73ee93e3e168e3375cebc17afb93b3ba5e
Tony Su <peitaosu@163.com>
2018-03-09 22:59:16 +0800

    correct the binary string format

--- Tool/Reg2JSON.py
+++ Tool/Reg2JSON.py
@@ -41,7 +41,7 @@ def convert_reg_str_to_json(reg_str_list):
                 value_data = value_content.split(':')[1]
             elif value_content.startswith('hex'):
                 value_type = "REG_BINARY"
-                value_data = value_content.split(':')[1]
+                value_data = value_content.split(':')[1].replace(',', '')
             cur_key['Values'].append(
                 {
                     "Name": value_name,
