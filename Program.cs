634c6631e54f5cf1d534d0b0afac015f72253e8a
Tony Su <peitaosu@163.com>
2018-03-18 21:41:59 +0800

    add FSHook into main

--- Program.cs
+++ Program.cs
@@ -19,39 +19,64 @@ namespace WinFSRegRedirector {
                 return;
 
             // Create the IPC server using the RegHook.ServiceInterface class as a singleton
-            EasyHook.RemoteHooking.IpcCreateServer<RegHook.ServerInterface> (ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton);
+            EasyHook.RemoteHooking.IpcCreateServer<RegHook.ServerInterface>(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton);
+
+            // Create the IPC server using the FSHook.ServiceInterface class as a singleton
+            EasyHook.RemoteHooking.IpcCreateServer<FSHook.ServerInterface>(ref channelName, System.Runtime.Remoting.WellKnownObjectMode.Singleton);
 
             // Get the full path to the assembly we want to inject into the target process
-            string injectionLibrary = Path.Combine (Path.GetDirectoryName (System.Reflection.Assembly.GetExecutingAssembly ().Location), "RegHook.dll");
+            string injectionRegLibrary = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "RegHook.dll");
+            string injectionFSLibrary = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "FSHook.dll");
 
-            try {
+            try
+            {
                 // Injecting into existing process by Id
                 if (targetPID > 0) {
                     Console.WriteLine ("Attempting to inject into process {0}", targetPID);
 
                     // inject into existing process
-                    EasyHook.RemoteHooking.Inject (
+                    EasyHook.RemoteHooking.Inject(
                         targetPID, // ID of process to inject into
-                        injectionLibrary, // 32-bit library to inject (if target is 32-bit)
-                        injectionLibrary, // 64-bit library to inject (if target is 64-bit)
+                        injectionRegLibrary, // 32-bit library to inject (if target is 32-bit)
+                        injectionRegLibrary, // 64-bit library to inject (if target is 64-bit)
                         channelName // the parameters to pass into injected library
-                        // ...
+                                    // ...
+                    );
+                    // inject into existing process
+                    EasyHook.RemoteHooking.Inject(
+                        targetPID, // ID of process to inject into
+                        injectionFSLibrary, // 32-bit library to inject (if target is 32-bit)
+                        injectionFSLibrary, // 64-bit library to inject (if target is 64-bit)
+                        channelName // the parameters to pass into injected library
+                                    // ...
                     );
                 }
                 // Create a new process and then inject into it
                 else if (!string.IsNullOrEmpty (targetExe)) {
                     Console.WriteLine ("Attempting to create and inject into {0}", targetExe);
                     // start and inject into a new process
-                    EasyHook.RemoteHooking.CreateAndInject (
+                    EasyHook.RemoteHooking.CreateAndInject(
                         targetExe, // executable to run
                         targetArg, // command line arguments for target
                         0, // additional process creation flags to pass to CreateProcess
                         EasyHook.InjectionOptions.DoNotRequireStrongName, // allow injectionLibrary to be unsigned
-                        injectionLibrary, // 32-bit library to inject (if target is 32-bit)
-                        injectionLibrary, // 64-bit library to inject (if target is 64-bit)
+                        injectionRegLibrary, // 32-bit library to inject (if target is 32-bit)
+                        injectionRegLibrary, // 64-bit library to inject (if target is 64-bit)
                         out targetPID, // retrieve the newly created process ID
                         channelName // the parameters to pass into injected library
-                        // ...
+                                    // ...
+                    );
+                    // start and inject into a new process
+                    EasyHook.RemoteHooking.CreateAndInject(
+                        targetExe, // executable to run
+                        targetArg, // command line arguments for target
+                        0, // additional process creation flags to pass to CreateProcess
+                        EasyHook.InjectionOptions.DoNotRequireStrongName, // allow injectionLibrary to be unsigned
+                        injectionFSLibrary, // 32-bit library to inject (if target is 32-bit)
+                        injectionFSLibrary, // 64-bit library to inject (if target is 64-bit)
+                        out targetPID, // retrieve the newly created process ID
+                        channelName // the parameters to pass into injected library
+                                    // ...
                     );
                 }
             } catch (Exception e) {
