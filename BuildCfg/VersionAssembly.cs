4c721dd8268276e373aabeca78b54df6cc96f253
Tony Su <peitaosu@163.com>
2018-11-22 21:21:30 +0800

    more common configuration

---
+++ BuildCfg/VersionAssembly.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Windows File System & Registry Redirector")]
+[assembly: AssemblyDescription("Windows File System & Registry Redirector")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Windows File System & Registry Redirector")]
+[assembly: AssemblyCopyright("Copyright © Tony Su 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: ComVisible(false)]
