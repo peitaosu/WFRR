e8a1b33c91ef0ae514dd9532d46fdf88ada244e3
Tony Su <peitaosu@163.com>
2018-11-24 02:02:07 +0800

    add RegCreateKeyEx implementations

--- RegHook/WinAPI.cs
+++ RegHook/WinAPI.cs
@@ -27,15 +27,69 @@ namespace RegHook
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Auto, SetLastError = true)]
         internal delegate IntPtr RegCreateKeyEx_Delegate(
-            IntPtr hKey,
-            string subKey,
-            ref IntPtr hkResult);
+            IntPtr hKey, 
+            string lpSubKey, 
+            IntPtr Reserved, 
+            string lpClass, 
+            RegOption dwOptions, 
+            RegSAM samDesired, 
+            ref SECURITY_ATTRIBUTES lpSecurityAttributes, 
+            out IntPtr phkResult, 
+            out RegResult lpdwDisposition);
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        [Flags]
+        public enum RegOption
+        {
+            NonVolatile = 0x0,
+            Volatile = 0x1,
+            CreateLink = 0x2,
+            BackupRestore = 0x4,
+            OpenLink = 0x8
+        }
+
+        [Flags]
+        public enum RegSAM
+        {
+            QueryValue = 0x0001,
+            SetValue = 0x0002,
+            CreateSubKey = 0x0004,
+            EnumerateSubKeys = 0x0008,
+            Notify = 0x0010,
+            CreateLink = 0x0020,
+            WOW64_32Key = 0x0200,
+            WOW64_64Key = 0x0100,
+            WOW64_Res = 0x0300,
+            Read = 0x00020019,
+            Write = 0x00020006,
+            Execute = 0x00020019,
+            AllAccess = 0x000f003f
+        }
+
+        public enum RegResult
+        {
+            CreatedNewKey = 0x00000001,
+            OpenedExistingKey = 0x00000002
+        }
 
         [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode, EntryPoint = "RegCreateKeyEx")]
         internal static extern IntPtr RegCreateKeyEx(
-            IntPtr hKey,
-            string subKey,
-            ref IntPtr hkResult);
+            IntPtr hKey, 
+            string lpSubKey, 
+            IntPtr Reserved, 
+            string lpClass, 
+            RegOption dwOptions, 
+            RegSAM samDesired, 
+            ref SECURITY_ATTRIBUTES lpSecurityAttributes, 
+            out IntPtr phkResult, 
+            out RegResult lpdwDisposition);
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Auto, SetLastError = true)]
         internal delegate IntPtr RegDeleteKeyEx_Delegate(
